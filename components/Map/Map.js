a64f9bd11ef1ebf21adab169a4f769ecb44e0c4e
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-11-30 16:19:11 +0100

    Deprecate sheets integration

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useMemo, useState } from "react";
-import GoogleMapReact from "google-map-react";
-import { Pin } from "./Pin";
-import { Box, Container } from "@chakra-ui/layout";
-import Carousel from "../Carousel/Carousel";
-import maricaGaj from "../../images/marica-gaj.jpeg";
-import church from "../../images/church.jpeg";
-import sirokiBrijeg from "../../images/siroki-brijeg.jpeg";
-import { useTranslation } from "next-i18next";
+import React, { useEffect, useMemo, useState } from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Pin } from './Pin'
+import { Box, Container } from '@chakra-ui/layout'
+import Carousel from '../Carousel/Carousel'
+import maricaGaj from '../../images/marica-gaj.jpeg'
+import church from '../../images/church.jpeg'
+import sirokiBrijeg from '../../images/siroki-brijeg.jpeg'
+import { useTranslation } from 'next-i18next'
 
 const defaultProps = {
   center: [43.387034744817086, 17.59122205063004],
   zoom: 12,
-};
+}
 
 const options = {
   panControl: true,
@@ -19,44 +19,44 @@ const options = {
   scrollwheel: false,
   // draggable: false,
   fullscreenControl: false,
-};
+}
 
 const createLocations = (t) => [
   {
     lat: 43.387034744817086,
     lng: 17.59122205063004,
-    title: t("parentsTitle"),
-    description: t("parentsDescription"),
+    title: t('parentsTitle'),
+    description: t('parentsDescription'),
     image: sirokiBrijeg,
   },
   {
     lat: 43.37473432038884,
     lng: 17.58928294603564,
-    title: t("churchTitle"),
-    description: t("churchDescription"),
+    title: t('churchTitle'),
+    description: t('churchDescription'),
     image: church,
   },
   {
     lat: 43.3394754,
     lng: 17.43006,
-    title: t("partyTitle"),
-    description: t("partyDescription"),
+    title: t('partyTitle'),
+    description: t('partyDescription'),
     image: maricaGaj,
   },
-];
+]
 
 export default function Map() {
-  const { t } = useTranslation("destination");
-  const locations = useMemo(() => createLocations(t), [t]);
-  const [selectedLocation, setSelectedLocation] = useState(0);
-  const [mapCenter, setMapCenter] = useState(defaultProps.center);
+  const { t } = useTranslation('destination')
+  const locations = useMemo(() => createLocations(t), [t])
+  const [selectedLocation, setSelectedLocation] = useState(0)
+  const [mapCenter, setMapCenter] = useState(defaultProps.center)
 
   useEffect(() => {
     setMapCenter([
       locations[selectedLocation].lat,
       locations[selectedLocation].lng,
-    ]);
-  }, [selectedLocation]);
+    ])
+  }, [selectedLocation])
 
   return (
     <Box mb="-6rem">
@@ -69,31 +69,33 @@ export default function Map() {
       </Container>
       <div
         style={{
-          height: "40vh",
-          width: "100%",
-          borderTop: "1px solid rgba(0,0,0,0.2)",
-          borderBottom: "1px solid rgba(0,0,0,0.2)",
+          height: '40vh',
+          width: '100%',
+          borderTop: '1px solid rgba(0,0,0,0.2)',
+          borderBottom: '1px solid rgba(0,0,0,0.2)',
         }}
       >
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY }}
-          center={mapCenter}
-          defaultZoom={defaultProps.zoom}
-          options={options}
-        >
-          {locations.map((location, locationIndex) => (
-            <Pin
-              key={location.title}
-              lat={location.lat}
-              lng={location.lng}
-              isSelected={selectedLocation === locationIndex}
-              onSelect={() => {
-                setSelectedLocation(locationIndex);
-              }}
-            />
-          ))}
-        </GoogleMapReact>
+        {process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY && (
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY }}
+            center={mapCenter}
+            defaultZoom={defaultProps.zoom}
+            options={options}
+          >
+            {locations.map((location, locationIndex) => (
+              <Pin
+                key={location.title}
+                lat={location.lat}
+                lng={location.lng}
+                isSelected={selectedLocation === locationIndex}
+                onSelect={() => {
+                  setSelectedLocation(locationIndex)
+                }}
+              />
+            ))}
+          </GoogleMapReact>
+        )}
       </div>
     </Box>
-  );
+  )
 }
--- pages/api/rsvp/[id].js
+++ pages/api/rsvp/[id].js
@@ -1,24 +1,24 @@
-const { GoogleSpreadsheet } = require("google-spreadsheet");
+const { GoogleSpreadsheet } = require('google-spreadsheet')
 
-const SPREADSHEET_ID = "1YSSYKx_tjzl2HJwc22IE078o8I1pi-erIXi0UnOPauE";
-const SHEET_TITLE = "rsvp";
+const SPREADSHEET_ID = '1YSSYKx_tjzl2HJwc22IE078o8I1pi-erIXi0UnOPauE'
+const SHEET_TITLE = 'rsvp'
 
-const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+const doc = new GoogleSpreadsheet(SPREADSHEET_ID)
 
 export default async function rsvp(req, res) {
   switch (req.method) {
-    case "GET": {
+    case '': {
       await doc.useServiceAccountAuth({
         client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
         private_key: process.env.GOOGLE_PRIVATE_KEY,
-      });
-      await doc.loadInfo(); // loads document properties and worksheets
+      })
+      await doc.loadInfo() // loads document properties and worksheets
 
-      const { id } = req.query;
+      const { id } = req.query
 
-      const sheet = doc.sheetsByTitle[SHEET_TITLE];
-      const rows = await sheet.getRows();
-      const existingRow = rows.find((row) => row.id === id);
+      const sheet = doc.sheetsByTitle[SHEET_TITLE]
+      const rows = await sheet.getRows()
+      const existingRow = rows.find((row) => row.id === id)
 
       if (existingRow) {
         const {
@@ -34,7 +34,7 @@ export default async function rsvp(req, res) {
           number_of_children,
           partner_name,
           allergies,
-        } = existingRow;
+        } = existingRow
         return res.status(200).json({
           id,
           status,
@@ -48,13 +48,13 @@ export default async function rsvp(req, res) {
           number_of_children,
           partner_name,
           allergies,
-        });
+        })
       } else {
-        res.status(404).end();
+        res.status(404).end()
       }
     }
     default: {
-      return res.status(405).end();
+      return res.status(405).end()
     }
   }
 }
